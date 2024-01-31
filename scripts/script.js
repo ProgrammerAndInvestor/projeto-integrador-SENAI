b7f697df9940f172771dac25bd0666acc601d4cc
Samuel Leite Pereira <131712358+ProgrammerAndInvestor@users.noreply.github.com>
2024-01-31 20:55:29 -0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,8 @@
 const hamburguer = document.querySelector(".hamburguer");
 const nav = document.querySelector(".navigation_bar");
+const body = document.body;
 
-hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
+hamburguer.addEventListener("click", () => {
+  nav.classList.toggle("active");
+    body.classList.toggle("active")
+});
